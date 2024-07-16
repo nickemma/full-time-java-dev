28dfc773305964e106b4a5fc5e877c24896af057
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-16 16:17:42 +0100

    learned about interfaces and multiple inheritance for both abstraction and implements with defaults medthod

--- src/main/java/interfaces/Product.java
+++ src/main/java/interfaces/Product.java
@@ -4,4 +4,9 @@ public interface Product {
 
     String getName();
     void setName(String name);
+
+    default double getPrice(){
+        return 0;
+    };
+    default void setPrice(double price){};
 }
